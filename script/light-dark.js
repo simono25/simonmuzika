eecab8acb86e4bf35bdb625a7f9235ed0f6eb842
Simon <frenettesimon7@gmail.com>
2021-11-22 20:41:41 -0500

    update

--- script/light-dark.js
+++ script/light-dark.js
@@ -1,23 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const dmbutton2 = document.getElementById("dmbutton2");
-  const dmbutton = document.getElementById("dmbutton");
-  const navdesktop = document.getElementById("navdesktop");
-
-  let darkmode = false;
-
-  dmbutton.addEventListener("click", function() {
-    console.log();
-    darkmode = !darkmode;
-    navdesktop.style.backgroundColor = "#292929";
-    dmbutton2.style.display = "block";
-    dmbutton.style.display = "none";
-  });
-
-  dmbutton2.addEventListener("click", function (e) {
-    console.log(e);
-    darkmode = !darkmode;
-    navdesktop.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
-    dmbutton2.style.display = "none";
-    dmbutton.style.display = "block";
-  });
-});
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
