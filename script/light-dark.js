f556f0f9f2aa876c50a24bfdb84013fc5340c266
Simon <frenettesimon7@gmail.com>
2021-11-23 03:17:55 -0500

    update

--- script/light-dark.js
+++ script/light-dark.js
@@ -1,3 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const btn1 = document.getElementById("btn1");
+
+  let darkmode = false;
+
+  btn1.addEventListener("click", function (e) {
+    console.log(e);
+    if (darkmode === false) {
+      btn1.style.backgroundImage = "url('images/sun.png')";
+      darkmode = true;
+    } else if (darkmode === true) {
+      btn1.style.backgroundImage = "url('images/moon.png')";
+      darkmode = false;
+    }
+  });
+});
+
 function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
